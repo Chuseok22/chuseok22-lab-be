2543a98f91a37da786cd6e802a12290b769e24f8
Chuseok22 <bjh59629@naver.com>
2025-04-07 16:23:41 +0900

    [기능추가][이슈템플릿] Github 브랜치 및 커밋 명 생성을 위한 issue-helper 구현 feat: 미사용클래스 ErrorDetail.java 삭제 및 Validation 예외 핸들링 작성 #8

--- src/main/java/com/chuseok22/lab/global/exception/controller/GlobalExceptionHandler.java
+++ src/main/java/com/chuseok22/lab/global/exception/controller/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.chuseok22.lab.global.exception.controller;
 
-import com.romrom.romback.global.exception.CustomException;
-import com.romrom.romback.global.exception.ErrorCode;
-import com.romrom.romback.global.exception.ErrorResponse;
+import com.chuseok22.lab.global.exception.CustomException;
+import com.chuseok22.lab.global.exception.ErrorCode;
+import com.chuseok22.lab.global.exception.ErrorResponse;
+import com.chuseok22.lab.global.exception.ValidErrorResponse;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -30,7 +35,30 @@ public class GlobalExceptionHandler {
   }
 
   /**
-   * 2) 그 외 예외 처리
+   * 2) Validation 예외 처리
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ValidErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+    log.error("ValidationException 발생: {}", e.getMessage(), e);
+    // Validation 에러 정보를 담을 Map 생성
+    Map<String, String> validation = new HashMap<>();
+    for (FieldError fieldError : e.getFieldErrors()) {
+      validation.put(fieldError.getField(), fieldError.getDefaultMessage());
+    }
+
+    // 공통 응답 DTO를 활용해 반환
+    // ErrorCode.INVALID_REQUEST -> 400
+    ValidErrorResponse response = ValidErrorResponse.builder()
+        .errorCode(HttpStatus.BAD_REQUEST.toString())
+        .errorMessage("잘못된 요청입니다.")
+        .validation(validation)
+        .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+  }
+
+  /**
+   * 3) 그 외 예외 처리
    */
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleException(Exception e) {
