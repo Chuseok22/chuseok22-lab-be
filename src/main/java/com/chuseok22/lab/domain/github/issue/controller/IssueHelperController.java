a0b7df5e3b2f8d66930e16d4f980b647ff629049
Chuseok22 <bjh59629@naver.com>
2025-04-16 13:52:59 +0900

    Github 브랜치 및 커밋 명 생성을 위한 issue-helper 구현 : feat : Github Issue 파싱 컨트롤러 구현 https://github.com/Chuseok22/chuseok22-lab-be/issues/8

--- src/main/java/com/chuseok22/lab/domain/github/issue/controller/IssueHelperController.java
+++ src/main/java/com/chuseok22/lab/domain/github/issue/controller/IssueHelperController.java
@@ -1,14 +1,17 @@
 package com.chuseok22.lab.domain.github.issue.controller;
 
+import com.chuseok22.lab.domain.auth.dto.CustomUserDetails;
 import com.chuseok22.lab.domain.github.issue.dto.IssueRequest;
 import com.chuseok22.lab.domain.github.issue.dto.IssueResponse;
 import com.chuseok22.lab.domain.github.issue.service.IssueHelperService;
+import com.chuseok22.lab.domain.member.domain.Member;
 import com.chuseok22.lab.global.aspect.LogMonitoringInvocation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +33,9 @@ public class IssueHelperController implements IssueHelperControllerDocs {
   @PostMapping(value = "")
   @LogMonitoringInvocation
   public ResponseEntity<IssueResponse> processIssueHelper(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails,
       @RequestBody @Valid IssueRequest request) {
-    return ResponseEntity.ok(issueHelperService.processIssueHelper(request));
+    Member member = customUserDetails.getMember();
+    return ResponseEntity.ok(issueHelperService.processIssueHelper(member, request));
   }
 }
